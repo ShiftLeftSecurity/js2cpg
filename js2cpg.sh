cb845a128fcbf97fec066664d3e87974a6bb8585
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2021-09-15 13:58:40 +0200

    Update js2cpg.sh

--- js2cpg.sh
+++ js2cpg.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-scriptpath="$dir/js2cpg/target/universal/stage/bin/js2cpg"
+scriptpath="$dir/target/universal/stage/bin/js2cpg"
 
 $scriptpath \
-    -Dlog4j.configurationFile=$dir/js2cpg/src/main/resources/log4j2.xml \
+    -Dlog4j.configurationFile=$dir/src/main/resources/log4j2.xml \
     $@
