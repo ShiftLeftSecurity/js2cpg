de4106dcdf9e268361d317416db7a59264bc2a05
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2021-09-15 13:33:01 +0200

    To be truly OS independent

--- js2cpg.sh
+++ js2cpg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-SCRIPT_ABS_PATH=$(readlink -f "$0")
+SCRIPT_ABS_PATH=$( cd "$(dirname "$0")" ; pwd -P )
 SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
 
 $SCRIPT_ABS_DIR/target/universal/stage/bin/js2cpg -Dlog4j.configurationFile=$SCRIPT_ABS_DIR/src/main/resources/log4j2.xml $@
