a0c6631f211c90d36c0972edbdb159750e0de2d0
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2021-09-15 15:29:16 +0200

    Update IntegrationTestFixture.scala

--- cfgIntegrationTests/src/test/scala/io/shiftleft/js2cpg/cpg/passes/IntegrationTestFixture.scala
+++ cfgIntegrationTests/src/test/scala/io/shiftleft/js2cpg/cpg/passes/IntegrationTestFixture.scala
@@ -30,7 +30,7 @@ object IntegrationTestFixture {
       val systemString = System.getProperty(osNameProperty)
       val (shellPrefix, cmd) =
         if (systemString != null && systemString.startsWith(windowsSystemPrefix)) {
-          ("bash" :: Nil, command.replace("./", ""))
+          ("sh" :: "-c" :: Nil, command.replace("./", ".\"))
         } else {
           ("sh" :: "-c" :: Nil, command)
         }
