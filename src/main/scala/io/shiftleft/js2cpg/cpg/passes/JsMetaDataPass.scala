bff6d0df64dc0d93ca060d5cd89a1d3c7d9c81ef
Max Leuthäuser <max.leuthaeuser@gmail.com>
2021-10-04 09:05:00 +0200

    Added hash to CPG metadata
    The hash is calculated as a checksum of all *.js input files.
    This is done after transpiling. Otherwise, it may happen, that we end up with
    no files for the hash calculation at all (e.g., for TS projects).

--- src/main/scala/io/shiftleft/js2cpg/cpg/passes/JsMetaDataPass.scala
+++ src/main/scala/io/shiftleft/js2cpg/cpg/passes/JsMetaDataPass.scala
@@ -1,11 +1,13 @@
 package io.shiftleft.js2cpg.cpg.passes
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.{Languages, nodes}
+import io.shiftleft.codepropertygraph.generated.Languages
+import io.shiftleft.codepropertygraph.generated.nodes.NewMetaData
 import io.shiftleft.passes.{CpgPass, DiffGraph, KeyPool}
 import org.slf4j.LoggerFactory
 
-class JsMetaDataPass(cpg: Cpg, keyPool: KeyPool) extends CpgPass(cpg, keyPool = Some(keyPool)) {
+class JsMetaDataPass(cpg: Cpg, keyPool: KeyPool, hash: String)
+    extends CpgPass(cpg, keyPool = Some(keyPool)) {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
@@ -13,7 +15,7 @@ class JsMetaDataPass(cpg: Cpg, keyPool: KeyPool) extends CpgPass(cpg, keyPool = 
     logger.debug(s"Generating meta-data.")
 
     val diffGraph = DiffGraph.newBuilder
-    val metaNode  = nodes.NewMetaData().language(Languages.JAVASCRIPT)
+    val metaNode  = NewMetaData().language(Languages.JAVASCRIPT).hash(hash)
     diffGraph.addNode(metaNode)
     Iterator(diffGraph.build())
   }
