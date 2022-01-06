dcd9916e2ff070760d89e511c0a2367c64cc5044
Markus Lottmann <markus@shiftleft.io>
2022-01-06 19:25:34 +0100

    Replace play json library with jackson library.
    Reason: No Scala 3 support for play json in sight any time soon.

--- src/main/scala/io/shiftleft/js2cpg/parser/TsConfigJsonParser.scala
+++ src/main/scala/io/shiftleft/js2cpg/parser/TsConfigJsonParser.scala
@@ -1,12 +1,13 @@
 package io.shiftleft.js2cpg.parser
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.shiftleft.js2cpg.io.ExternalCommand
 import io.shiftleft.js2cpg.preprocessing.TypescriptTranspiler._
 import org.slf4j.LoggerFactory
-import play.api.libs.json.Json
 
 import java.nio.file.Path
 import scala.util.{Failure, Success}
+import scala.jdk.CollectionConverters._
 
 object TsConfigJsonParser {
 
@@ -15,9 +16,11 @@ object TsConfigJsonParser {
   def module(projectPath: Path, tsc: String): String = {
     ExternalCommand.run(s"${ExternalCommand.toOSCommand(tsc)} --showConfig", projectPath.toString) match {
       case Success(tsConfig) =>
-        val json = Json.parse(tsConfig)
-        val moduleOption = (json \ "compilerOptions" \ "module")
-          .asOpt[String]
+        val json = new ObjectMapper().readTree(tsConfig)
+        val moduleOption =
+          Option(json.get("compilerOptions"))
+            .flatMap(compOption => Option(compOption.get("module")))
+            .map(_.asText())
         moduleOption match {
           case Some(module) if module == ESNEXT || module == ES2020 => ESNEXT
           case _                                                    => DEFAULT_MODULE
@@ -45,12 +48,12 @@ object TsConfigJsonParser {
   def subprojects(projectPath: Path, tsc: String): List[String] = {
     ExternalCommand.run(s"${ExternalCommand.toOSCommand(tsc)} --showConfig", projectPath.toString) match {
       case Success(config) =>
-        val json = Json.parse(config)
-
-        (json \ "references")
-          .asOpt[List[Map[String, String]]]
-          .getOrElse(Nil)
-          .flatMap(_.get("path"))
+        val json = new ObjectMapper().readTree(config)
+        val referenceIt =
+          Option(json.get("references")).map(_.elements().asScala).getOrElse(Iterator.empty)
+        referenceIt.flatMap { reference =>
+          Option(reference.get("path")).map(_.asText)
+        }.toList
 
       case Failure(exception) =>
         logger.debug(
