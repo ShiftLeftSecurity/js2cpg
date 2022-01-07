6a6df90e6565f27c2662808dbf5d25f97863ad90
Markus Lottmann <markus@shiftleft.io>
2022-01-07 11:41:10 +0100

    Enable Scala 3 cross build.

--- src/main/scala/io/shiftleft/js2cpg/core/Js2CpgMain.scala
+++ src/main/scala/io/shiftleft/js2cpg/core/Js2CpgMain.scala
@@ -1,15 +1,16 @@
 package io.shiftleft.js2cpg.core
 
-object Js2CpgMain extends App {
+object Js2CpgMain {
+  def main(args: Array[String]): Unit = {
+    val argumentsParser: Js2cpgArgumentsParser = new Js2cpgArgumentsParser()
 
-  private val argumentsParser: Js2cpgArgumentsParser = new Js2cpgArgumentsParser()
+    argumentsParser.parse(args) match {
+      case Some(config) =>
+        new Js2Cpg().run(config)
+      case None =>
+        argumentsParser.showUsage()
+        System.exit(1)
+    }
 
-  argumentsParser.parse(args) match {
-    case Some(config) =>
-      new Js2Cpg().run(config)
-    case None =>
-      argumentsParser.showUsage()
-      System.exit(1)
   }
-
 }
