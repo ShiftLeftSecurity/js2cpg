8b4f6eab3e185e1744fc614d01554173cf6e4748
Malte Kraus <mkraus@shiftleft.io>
2022-01-12 16:51:18 +0100

    sync call linker

--- src/main/scala/io/shiftleft/js2cpg/cpg/passes/CallLinkerPass.scala
+++ src/main/scala/io/shiftleft/js2cpg/cpg/passes/CallLinkerPass.scala
@@ -1,32 +1,34 @@
 package io.shiftleft.js2cpg.cpg.passes
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.{nodes, DispatchTypes, EdgeTypes, Operators}
+import io.shiftleft.codepropertygraph.generated.{DispatchTypes, EdgeTypes, Operators, nodes}
 import io.shiftleft.passes.{CpgPass, DiffGraph}
 import io.shiftleft.semanticcpg.language._
-import overflowdb.traversal._
+import org.slf4j.{Logger, LoggerFactory}
+import overflowdb.traversal.{NodeOps, jIteratortoTraversal}
 
 import scala.collection.mutable
 
 /**
-  * This pass links call sites (by full name) and call sites to methods in the same file (by name).
+  * The Javascript specific call linker links static call sites (by full name) and
+  * call sites to methods in the same file (by name).
   */
 class CallLinkerPass(cpg: Cpg) extends CpgPass(cpg) {
-
-  private val JS_EXPORT_NAMES = IndexedSeq("module.exports", "exports")
+  import CallLinkerPass.logger
 
   private type MethodsByNameAndFileType = mutable.HashMap[(String, String), nodes.Method]
   private type MethodsByFullNameType    = mutable.HashMap[String, nodes.Method]
 
   private def isStaticSingleAssignmentLocal(ident: nodes.Identifier): Boolean =
     ident.refsTo
-      .filter(_.isInstanceOf[nodes.Local])
-      .cast[nodes.Local]
+      .collectAll[nodes.Local]
       .referencingIdentifiers
       .argumentIndex(1)
       .inCall
-      .name(Operators.assignment + ".*")
+      .assignments
       .size == 1
+
+  private val JS_EXPORT_NAMES = IndexedSeq("module.exports", "exports")
 
   private def createMethodsByNameAndFile(): (MethodsByNameAndFileType, MethodsByFullNameType) = {
     val methodsByNameAndFile = new MethodsByNameAndFileType()
@@ -46,18 +48,21 @@ class CallLinkerPass(cpg: Cpg) extends CpgPass(cpg) {
         .inCall
         .nameExact(Operators.assignment)
         .argument(1)
-        .collectFirst {
+        .flatMap {
           case assignee: nodes.Identifier
               if isStaticSingleAssignmentLocal(assignee) && assignee.method.name == ":program" =>
-            assignee.name
+            Some(assignee.name)
           case assignee: nodes.Call
               if assignee.methodFullName == Operators.fieldAccess &&
                 JS_EXPORT_NAMES.contains(assignee.argument(1).code) =>
-            assignee
-              .argument(2)
-              .asInstanceOf[nodes.FieldIdentifier]
-              .canonicalName
+            Some(
+              assignee
+                .argument(2)
+                .asInstanceOf[nodes.FieldIdentifier]
+                .canonicalName)
+          case _ => None
         }
+        .headOption
         .foreach { name =>
           methodsByNameAndFile.put((method.filename, name), method)
         }
@@ -68,38 +73,77 @@ class CallLinkerPass(cpg: Cpg) extends CpgPass(cpg) {
 
   override def run(): Iterator[DiffGraph] = {
     val (methodsByNameAndFileType, methodsByFullName) = createMethodsByNameAndFile()
-    val diffGraph                                     = linkCallsites(methodsByNameAndFileType, methodsByFullName)
+
+    val diffGraph = linkCallsites(methodsByNameAndFileType, methodsByFullName)
+
     Iterator(diffGraph)
   }
 
   private def linkCallsites(methodsByNameAndFile: MethodsByNameAndFileType,
                             methodsByFullName: MethodsByFullNameType): DiffGraph = {
     val diffGraph = DiffGraph.newBuilder
+
     cpg.call.foreach { call =>
       if (call.dispatchType == DispatchTypes.STATIC_DISPATCH) {
-        methodsByFullName.get(call.methodFullName).foreach { method =>
+        for (method <- methodsByFullName.get(call.methodFullName)) {
           diffGraph.addEdgeInOriginal(call, method, EdgeTypes.CALL)
         }
       } else {
-        getReceiverIdentifierName(call).foreach { name =>
-          for (file   <- call.file.headOption;
-               method <- methodsByNameAndFile.get((file.name, name))) {
-            diffGraph.addEdgeInOriginal(call, method, EdgeTypes.CALL)
-          }
+        getReceiverIdentifierName(call) match {
+          case Some(name) =>
+            try {
+              for (method <- methodsByNameAndFile.get((call.method.filename, name))) {
+                diffGraph.addEdgeInOriginal(call, method, EdgeTypes.CALL)
+              }
+            } catch {
+              case nse: NoSuchElementException =>
+                // Temporary workaround for crash in #7484
+                logger.error(s"failed to link call site for $name", nse)
+            }
+          case None =>
         }
       }
     }
+
     diffGraph.build()
   }
-
   private def callReceiverOption(callNode: nodes.Call): Option[nodes.Expression] =
-    callNode._receiverOut.nextOption() map (_.asInstanceOf[nodes.Expression])
+    callNode._receiverOut.nextOption().map(_.asInstanceOf[nodes.Expression])
 
   // Obtain method name for dynamic calls where the receiver is an identifier.
   private def getReceiverIdentifierName(call: nodes.Call): Option[String] = {
-    callReceiverOption(call).collect {
-      case identifier: nodes.Identifier => identifier.name
+    def fromFieldAccess(c: nodes.Call) =
+      if (c.methodFullName == Operators.fieldAccess && JS_EXPORT_NAMES.contains(c.argument(1).code)) {
+        Some(c.argument(2).code)
+      } else {
+        None
+      }
+
+    callReceiverOption(call) match {
+      case Some(callReceiver) =>
+        callReceiver match {
+          case identifier: nodes.Identifier =>
+            Some(identifier.name)
+          case block: nodes.Block =>
+            block.astChildren.lastOption
+              .collect { case c: nodes.Call => c }
+              .flatMap(fromFieldAccess)
+          case call: nodes.Call =>
+            // TODO: remove this if, once we no longer care about compat with CPGs from January 2022 (comma operator is now a block)
+            if (call.methodFullName == "<operator>.commaright")
+              call.argumentOption(2).isCall.headOption.flatMap(fromFieldAccess)
+            else
+              fromFieldAccess(call)
+          case _ =>
+            None
+        }
+      case None =>
+        None
     }
   }
 
+}
+
+object CallLinkerPass {
+  private val logger: Logger = LoggerFactory.getLogger(classOf[CallLinkerPass])
 }
