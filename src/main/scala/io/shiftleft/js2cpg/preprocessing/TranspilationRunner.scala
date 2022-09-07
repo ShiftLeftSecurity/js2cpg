e1f1306416f0e553641932abb906e0db6ea51856
Malte Kraus <mkraus@shiftleft.io>
2022-09-07 12:54:41 +0200

    strip @babel dependencies with --optimize-dependencies

--- src/main/scala/io/shiftleft/js2cpg/preprocessing/TranspilationRunner.scala
+++ src/main/scala/io/shiftleft/js2cpg/preprocessing/TranspilationRunner.scala
@@ -23,7 +23,7 @@ class TranspilationRunner(projectPath: Path, tmpTranspileDir: Path, config: Conf
 
   private val transpilers: Seq[Transpiler] = createTranspilers()
 
-  private val DEPS_TO_KEEP: List[String] = List("@vue", "vue", "nuxt", "@babel")
+  private val DEPS_TO_KEEP: List[String] = List("@vue", "vue", "nuxt")
 
   private def createTranspilers(): Seq[Transpiler] = {
     // We always run the following transpilers by default when not stated otherwise in the Config.
