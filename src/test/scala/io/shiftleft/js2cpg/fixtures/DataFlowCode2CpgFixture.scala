25fc03714c896922820fd624a6a7605922b8f16d
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-11-03 09:50:39 +0100

    Fix for review comment

--- src/test/scala/io/shiftleft/js2cpg/fixtures/DataFlowCode2CpgFixture.scala
+++ src/test/scala/io/shiftleft/js2cpg/fixtures/DataFlowCode2CpgFixture.scala
@@ -10,11 +10,9 @@ import io.joern.x2cpg.testfixtures.TestCpg
 import io.joern.x2cpg.X2Cpg
 import io.joern.x2cpg.passes.frontend.JavascriptCallLinker
 import io.joern.x2cpg.testfixtures.LanguageFrontend
-import io.shiftleft.codepropertygraph.generated.nodes._
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.js2cpg.core.Config
 import io.shiftleft.js2cpg.core.Js2Cpg
-import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.layers.LayerCreatorContext
 
 trait Js2CpgFrontend extends LanguageFrontend {
@@ -59,18 +57,8 @@ class DataFlowCode2CpgFixture extends Code2CpgFixture(() => new DataFlowTestCpg(
 
   implicit lazy val context: EngineContext = EngineContext()
 
-  protected def flowToResultPairs(path: Path): List[(String, Integer)] = {
-    val pairs = path.elements.map {
-      case point: MethodParameterIn =>
-        val method      = point.method.head
-        val method_name = method.name
-        val code        = s"$method_name(${method.parameter.l.sortBy(_.order).map(_.code).mkString(", ")})"
-        (code, point.lineNumber.getOrElse(Int.box(-1)))
-      case point =>
-        (point.statement.repr, point.lineNumber.getOrElse(Int.box(-1)))
+  protected def flowToResultPairs(path: Path): List[(String, Integer)] =
+    path.resultPairs().collect { case (firstElement: String, secondElement: Option[Integer]) =>
+      (firstElement, secondElement.get)
     }
-    pairs.headOption
-      .map(x => x :: pairs.sliding(2).collect { case Seq(a, b) if a != b => b }.toList)
-      .getOrElse(List())
-  }
 }
