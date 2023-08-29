3709b9cefb0f5146022eec4cdc7cb43ebb6de32c
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2023-08-29 11:01:28 +0200

    Multiple fixes for vue transpilation (#293)
    * fixed browserslist filename
    * create custom vue.config.js (always enable sourcemaps, overwrite user-specific settings)
    * use --modern vue cli build flag to create ES2015+ code. Transpiled and polyfilled bundles for older ES versions are useless.
    * fixed vue transpilation dependencies for mixed TS/Vue.js projects. vue-cli does not support that out of the box if the dependencies are not installed explicitly. 
    
    For: https://shiftleftinc.atlassian.net/browse/SEN-1120

--- src/main/scala/io/shiftleft/js2cpg/preprocessing/VueTranspiler.scala
+++ src/main/scala/io/shiftleft/js2cpg/preprocessing/VueTranspiler.scala
@@ -4,31 +4,58 @@ import better.files.File
 import io.shiftleft.js2cpg.core.Config
 import io.shiftleft.js2cpg.io.ExternalCommand
 import io.shiftleft.js2cpg.io.FileDefaults
-import io.shiftleft.js2cpg.io.FileDefaults.VUE_SUFFIX
+import io.shiftleft.js2cpg.io.FileDefaults._
 import io.shiftleft.js2cpg.io.FileUtils
 import io.shiftleft.js2cpg.parser.PackageJsonParser
 import org.slf4j.LoggerFactory
 
 import java.nio.file.{Path, Paths}
 import scala.util.{Failure, Success}
+import scala.util.Try
 
 object VueTranspiler {
+
+  private val BrowsersListRc: String = ".browserslistrc"
+  private val VueConfigJs: String    = "vue.config.js"
+  private val Vue2Config: String =
+    """
+      |module.exports = {
+      |  configureWebpack: {
+      |    devtool: 'source-map'
+      |  }
+      |}
+      |""".stripMargin
+  private val Vue3Config: String =
+    """
+      |const { defineConfig } = require('@vue/cli-service');
+      |module.exports = defineConfig({
+      |  configureWebpack: {
+      |    devtool: 'source-map',
+      |  }
+      |});
+      |""".stripMargin
 
   private def hasVueFiles(config: Config, projectPath: Path): Boolean =
     FileUtils.getFileTree(projectPath, config, List(VUE_SUFFIX)).nonEmpty
 
   def isVueProject(config: Config, projectPath: Path): Boolean = {
     val hasVueDep =
-      PackageJsonParser
-        .dependencies((File(config.srcDir) / FileDefaults.PACKAGE_JSON_FILENAME).path)
-        .contains("vue")
+      PackageJsonParser.dependencies((File(config.srcDir) / FileDefaults.PACKAGE_JSON_FILENAME).path).contains("vue")
     hasVueDep && hasVueFiles(config, projectPath)
+  }
+
+  private def vueVersion(config: Config): Int = {
+    val versionString =
+      PackageJsonParser.dependencies((File(config.srcDir) / FileDefaults.PACKAGE_JSON_FILENAME).path)("vue")
+    // ignore ~, ^, and more from semver; see: https://stackoverflow.com/a/25861938
+    val c = versionString.collectFirst { case c if Try(c.toString.toInt).isSuccess => c.toString.toInt }
+    c.getOrElse(3) // 3 is the latest version; we default to that
   }
 }
 
 class VueTranspiler(override val config: Config, override val projectPath: Path) extends Transpiler {
 
-  import VueTranspiler.isVueProject
+  import VueTranspiler._
 
   private val logger = LoggerFactory.getLogger(getClass)
 
@@ -48,13 +75,17 @@ class VueTranspiler(override val config: Config, override val projectPath: Path)
     }
   }
 
+  private def hasTsFiles: Boolean =
+    FileUtils.getFileTree(projectPath, config, List(TS_SUFFIX)).nonEmpty
+
   private def installVuePlugins(): Boolean = {
+    val additionalPlugins = if (hasTsFiles) "typescript @vue/cli-plugin-typescript" else ""
     val command = if (pnpmAvailable(projectPath)) {
-      s"${TranspilingEnvironment.PNPM_ADD} $vueAndVersion && ${TranspilingEnvironment.PNPM_INSTALL}"
+      s"${TranspilingEnvironment.PNPM_ADD} $vueAndVersion $additionalPlugins && ${TranspilingEnvironment.PNPM_INSTALL}"
     } else if (yarnAvailable()) {
-      s"${TranspilingEnvironment.YARN_ADD} $vueAndVersion && ${TranspilingEnvironment.YARN_INSTALL}"
+      s"${TranspilingEnvironment.YARN_ADD} $vueAndVersion $additionalPlugins && ${TranspilingEnvironment.YARN_INSTALL}"
     } else {
-      s"${TranspilingEnvironment.NPM_INSTALL} $vueAndVersion"
+      s"${TranspilingEnvironment.NPM_INSTALL} $vueAndVersion $additionalPlugins"
     }
     logger.info("Installing Vue.js dependencies and plugins. That will take a while.")
     logger.debug(s"\t+ Installing Vue.js plugins with command '$command' in path '$projectPath'")
@@ -69,20 +100,29 @@ class VueTranspiler(override val config: Config, override val projectPath: Path)
   }
 
   private def createCustomBrowserslistFile(): Unit = {
-    val browserslistFile = File(projectPath) / ".browserslistrc"
-    if (browserslistFile.exists) {
-      browserslistFile.delete(swallowIOExceptions = true)
+    val browserslistFile = File(projectPath) / BrowsersListRc
+    browserslistFile.delete(swallowIOExceptions = true)
+    browserslistFile.createFile().deleteOnExit(swallowIOExceptions = true)
+    browserslistFile.writeText("last 2 years")
+  }
+
+  private def createCustomVueConfigFile(): Unit = {
+    val vueConfigJsFile = File(projectPath) / VueConfigJs
+    vueConfigJsFile.delete(swallowIOExceptions = true)
+    vueConfigJsFile.createFile().deleteOnExit(swallowIOExceptions = true)
+    if (vueVersion(config) <= 2) {
+      vueConfigJsFile.writeText(Vue2Config)
+    } else {
+      vueConfigJsFile.writeText(Vue3Config)
     }
-    val customBrowserslistFile = File
-      .newTemporaryFile(".browserslistrc", parent = Some(projectPath))
-      .deleteOnExit(swallowIOExceptions = true)
-    customBrowserslistFile.writeText("last 2 years")
   }
 
   override protected def transpile(tmpTranspileDir: Path): Boolean = {
     if (installVuePlugins()) {
       createCustomBrowserslistFile()
-      val command = s"${ExternalCommand.toOSCommand(vue)} build --dest '$tmpTranspileDir' --mode development --no-clean"
+      createCustomVueConfigFile()
+      val command =
+        s"${ExternalCommand.toOSCommand(vue)} build --dest '$tmpTranspileDir' --mode development --no-clean --modern"
       logger.debug(s"\t+ Vue.js transpiling $projectPath to '$tmpTranspileDir'")
       ExternalCommand.run(command, projectPath.toString, extraEnv = VUE_NODE_OPTIONS) match {
         case Success(_)         => logger.debug("\t+ Vue.js transpiling finished")
