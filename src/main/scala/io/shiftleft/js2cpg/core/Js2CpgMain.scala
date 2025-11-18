b9b4313099a842c6ac154d8739f27bc2cedb7cdd
maltek <1694194+maltek@users.noreply.github.com>
2025-11-18 11:09:38 +0100

    implement server mode, same as joern frontends (#368)

--- src/main/scala/io/shiftleft/js2cpg/core/Js2CpgMain.scala
+++ src/main/scala/io/shiftleft/js2cpg/core/Js2CpgMain.scala
@@ -1,9 +1,30 @@
 package io.shiftleft.js2cpg.core
 
+import io.joern.x2cpg.utils.server.FrontendHTTPServer
+
 object Js2CpgMain {
   def main(args: Array[String]): Unit = {
     val argumentsParser = new Js2cpgArgumentsParser()
     argumentsParser.parse(args) match {
+      case Some(config) if config.serverMode =>
+        val server: FrontendHTTPServer = new FrontendHTTPServer(
+          FrontendHTTPServer.defaultExecutor(),
+          arguments => {
+            val config = argumentsParser.parse(arguments)
+            config.foreach(Js2Cpg().run(_))
+          }
+        )
+
+        val port = server.startup()
+        println(s"FrontendHTTPServer started on port $port")
+        try {
+          config.serverTimeoutSeconds match {
+            case Some(value) => server.stopServerAfterTimeout(value)
+            case None        => Thread.sleep(Long.MaxValue)
+          }
+        } finally {
+          server.stop()
+        }
       case Some(config) =>
         new Js2Cpg().run(config)
       case None =>
